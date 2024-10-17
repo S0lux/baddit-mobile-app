c56f5132357d51a5e42d61b6e0a7243cf75ff705
VaderNgo <vaderngo@gmail.com>
2024-10-17 23:25:55 +0700

    Added navigation and bottom navigation bar

--- app/src/main/java/com/example/baddit/presentation/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/example/baddit/presentation/screens/home/HomeViewModel.kt
@@ -1,6 +1,9 @@
 package com.example.baddit.presentation.screens.home
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.baddit.domain.error.Result
