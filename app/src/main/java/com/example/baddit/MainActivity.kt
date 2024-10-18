71056e2faebce87e5542ed12b73b3f8ad43d1eea
VaderNgo <vaderngo@gmail.com>
2024-10-18 19:37:33 +0700

    Modified navigation

--- app/src/main/java/com/example/baddit/MainActivity.kt
+++ app/src/main/java/com/example/baddit/MainActivity.kt
@@ -5,8 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -15,93 +17,144 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
 import com.example.baddit.presentation.components.BottomNavigationbar
 import com.example.baddit.presentation.screens.community.CommunityScreen
 import com.example.baddit.presentation.screens.createPost.CreatePostScreen
 import com.example.baddit.presentation.screens.home.HomeScreen
 import com.example.baddit.presentation.screens.home.HomeViewModel
+import com.example.baddit.presentation.screens.signup.SignupScreen
+import com.example.baddit.presentation.utils.Auth
+import com.example.baddit.presentation.utils.Community
+import com.example.baddit.presentation.utils.CreatePost
+import com.example.baddit.presentation.utils.Home
+import com.example.baddit.presentation.utils.Main
+import com.example.baddit.presentation.utils.SignUp
 import com.example.baddit.ui.theme.BadditTheme
-import com.example.baddit.ui.theme.CustomTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.serialization.Serializable
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
-        
+
         super.onCreate(savedInstanceState)
-        installSplashScreen()
+        //installSplashScreen()
         setContent {
             val navController = rememberNavController()
+            var bottomBarState = rememberSaveable { mutableStateOf(true) }
+            val navBackStackEntry by navController.currentBackStackEntryAsState()
+
+            when (navBackStackEntry?.destination?.route) {
+                "com.example.baddit.presentation.utils.Home" -> {
+                    // Show BottomBar and TopBar
+                    bottomBarState.value = true
+                }
+
+                "com.example.baddit.presentation.utils.CreatePost" -> {
+                    // Show BottomBar and TopBar
+                    bottomBarState.value = true
+                }
+
+                "com.example.baddit.presentation.utils.Community" -> {
+                    // Show BottomBar and TopBar
+                    bottomBarState.value = true
+                }
+
+                "com.example.baddit.presentation.utils.SignUp" -> {
+                    // Hide BottomBar and TopBar
+                    bottomBarState.value = false
+                }
+            }
+
+
             BadditTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    SignupScreen()
-                }
-            }
-        }
                     Scaffold(
                         modifier = Modifier.fillMaxSize(),
                         bottomBar = {
-                            BottomNavigationbar(navController = navController)
+                            BottomNavigationbar(
+                                navController = navController,
+                                bottomBarState = bottomBarState
+                            )
                         }) { it ->
                         NavHost(
                             navController = navController,
-                            startDestination = Home,
+                            startDestination = Main,
                             modifier = Modifier.padding(it)
                         ) {
-                            composable<Home> {
-                                val homeViewModel: HomeViewModel = hiltViewModel()
-                                EnterAnimation {
-                                    HomeScreen(viewModel = homeViewModel)
+                            navigation<Main>(startDestination = Home) {
+                                composable<Home> {
+                                    val homeViewModel: HomeViewModel = hiltViewModel()
+                                    SlideHorizontally {
+                                        HomeScreen(viewModel = homeViewModel)
+                                    }
                                 }
-                            }
-                            composable<CreatePost>() {
-                                EnterAnimation {
-                                    CreatePostScreen()
+                                composable<CreatePost>() {
+                                    SlideHorizontally {
+                                        CreatePostScreen()
+                                    }
                                 }
-                            }
-                            composable<Community> {
-                                EnterAnimation {
-                                    CommunityScreen()
+                                composable<Community> {
+                                    SlideHorizontally {
+                                        CommunityScreen()
+                                    }
                                 }
                             }
 
+                            navigation<Auth>(startDestination = SignUp) {
+                                composable<SignUp> {
+                                    SlideVertically {
+                                        SignupScreen()
 
+                                    }
+                                }
+                            }
                         }
                     }
                 }
             }
         }
+
     }
 }
 
-@Serializable
-object Home
-
-@Serializable
-object CreatePost
-
-@Serializable
-object Community
 
 @Composable
-fun EnterAnimation(content: @Composable () -> Unit) {
+fun SlideHorizontally(content: @Composable () -> Unit) {
     AnimatedVisibility(
         visibleState = MutableTransitionState(
             initialState = false
         ).apply { targetState = true },
         modifier = Modifier,
         enter = slideInHorizontally(),
+        exit = fadeOut()
+    ) {
+        content()
+    }
+}
+
+@Composable
+fun SlideVertically(content: @Composable () -> Unit) {
+    AnimatedVisibility(
+        visibleState = MutableTransitionState(
+            initialState = false
+        ).apply { targetState = true },
+        modifier = Modifier,
+        enter = fadeIn() + slideInVertically(),
         exit = fadeOut() + slideOutHorizontally()
     ) {
         content()
--- app/src/main/java/com/example/baddit/presentation/components/BottomNavigationBar.kt
+++ app/src/main/java/com/example/baddit/presentation/components/BottomNavigationBar.kt
@@ -1,11 +1,13 @@
 package com.example.baddit.presentation.components
 
 import androidx.annotation.DrawableRes
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -14,15 +16,17 @@ import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavHostController
-import com.example.baddit.Community
-import com.example.baddit.CreatePost
-import com.example.baddit.Home
 import com.example.baddit.R
+import com.example.baddit.presentation.utils.Auth
+import com.example.baddit.presentation.utils.Community
+import com.example.baddit.presentation.utils.CreatePost
+import com.example.baddit.presentation.utils.Home
 
 
 @Composable
 fun BottomNavigationbar(
     navController: NavHostController,
+    bottomBarState: MutableState<Boolean>
 ) {
     var selectedIndex by remember { mutableStateOf(0) }
     val navitems = listOf(
@@ -32,26 +36,34 @@ fun BottomNavigationbar(
             icon = R.drawable.round_groups_24,
             value = Community
         ),
+        BottomNavigationItem(icon = R.drawable.round_person_24, value = Auth)
     )
 
-    NavigationBar {
-        navitems.forEachIndexed { index, item ->
-            NavigationBarItem(
-                selected = index.equals(selectedIndex),
-                onClick = {
-                    if (index != selectedIndex) {
-                        navController.navigate(item.value)
-                        selectedIndex = index
+    AnimatedVisibility(visible = bottomBarState.value) {
+        NavigationBar {
+            navitems.forEachIndexed { index, item ->
+                NavigationBarItem(
+                    selected = index.equals(selectedIndex),
+                    onClick = {
+                        if (index != selectedIndex) {
+                            navController.navigate(item.value)
+                            selectedIndex = index
+                        }
+                    },
+                    icon = {
+                        Column(modifier = Modifier, horizontalAlignment = CenterHorizontally) {
+                            Icon(
+                                painter = painterResource(id = item.icon),
+                                contentDescription = null
+                            )
+                        }
                     }
-                },
-                icon = {
-                    Column(modifier = Modifier, horizontalAlignment = CenterHorizontally) {
-                        Icon(painter = painterResource(id = item.icon), contentDescription = null)
-                    }
-                })
-
+                )
+            }
         }
     }
+
+
 }
 
 data class BottomNavigationItem(
